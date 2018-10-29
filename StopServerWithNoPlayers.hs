9939de00781fcf52c7407f46fcfdd84822860085
Mike MacHenry <mike.machenry@gmail.com>
2018-10-29 04:42:40 -0400

    Finishing conditional shutdown script.

--- StopServerWithNoPlayers.hs
+++ StopServerWithNoPlayers.hs
@@ -1,19 +1,39 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-12.5 --install-ghc runghc --package parsec
 module StopServerWithNoPlayers (main) where
 
-import Text.Parsec.String (Parser)
-import Text.Parsec.Char (string)
-import Text.Parsec (parse)
-import Text.Parsec.Number (nat)
 import System.Exit (die)
 import System.Process (readProcess, callProcess)
 
--- Use the list command to see if anyone is currently online. If someone
--- is online, do nothing. Otherwise, check the logs.
--- $ docker exec mc rcon-cli list
--- There are 0 of a max 20 players online:
+import Text.Parsec.String (Parser, parseFromFile)
+import Text.Parsec.Char (string, digit, char, endOfLine, anyChar, oneOf)
+import Text.Parsec (parse, many, many1)
+import Text.Parsec.Combinator (count, between, manyTill)
+
+import Data.Time.LocalTime (
+  LocalTime(..), TimeOfDay(..), zonedTimeToLocalTime, getZonedTime,
+  utcToLocalTime, utc, localTimeToUTC)
+import Data.Time.Calendar (fromGregorian)
+import Data.Time.Clock (addUTCTime)
+
+import Data.List (isInfixOf)
+
+data LogEntry = LogEntry {
+  logEntryDate :: LocalTime,
+  logEntryText :: String
+  }
 
 main :: IO ()
-main = getNumPlayersOnline >>= print
+main = do
+  n <- getNumPlayersOnline
+  if n > 0
+  then putStrLn "Players are online."
+  else do d <- getLastActiveDateTime "test.log"
+          putStrLn $ "Activity found at " ++ show d
+          now <- zonedTimeToLocalTime <$> getZonedTime
+          if addSeconds (60*10) d < now
+          then putStrLn "Stopping the server." >> stopServer
+          else putStrLn "Doing nothing."
 
 getNumPlayersOnline :: IO Int
 getNumPlayersOnline = do
@@ -23,20 +43,53 @@ getNumPlayersOnline = do
     Right numPlayers -> return numPlayers
 
 playerList :: Parser Int
-playerList =
-  string "There are " *> nat <*
-  (string " of a max " >> nat >> string " players online:")
+playerList = do
+  string "There are "
+  numStr <- many1 digit
+  string " of a max "
+  many1 digit
+  string " players online:"
+  return (read numStr)
+
+getLastActiveDateTime :: String -> IO LocalTime
+getLastActiveDateTime logFilename = do
+  result <- parseFromFile (many logLine) logFilename
+  case result of
+    Left err -> die (show err)
+    Right logLines ->
+      return $ maximum $ map logEntryDate $ filter isActivity logLines
+
+logLine :: Parser LogEntry
+logLine = do
+  dateTime <- between (char '[') (char ']') iso8601
+  message <- manyTill anyChar endOfLine
+  return $ LogEntry dateTime message
+
+iso8601 :: Parser LocalTime
+iso8601 = do
+  year <- count 4 digit
+  char '-'
+  month <- count 2 digit
+  char '-'
+  day <- count 2 digit
+  let calendarDay = fromGregorian (read year) (read month) (read day)
+  oneOf "T "
+  hour <- count 2 digit
+  char ':'
+  minute <- count 2 digit
+  char ':'
+  second <- count 2 digit
+  let timeOfDay = TimeOfDay (read hour) (read minute) (read second)
+  return (LocalTime calendarDay timeOfDay)
+
+isActivity :: LogEntry -> Bool
+isActivity (LogEntry _ text) = 
+  isInfixOf "Starting Minecraft server" text || isInfixOf "Disconnected" text
 
 stopServer :: IO ()
 stopServer = callProcess "/usr/bin/docker" ["exec", "mc", "rcon-cli", "stop"]
 
--- Check the logs for most recent player logout message, which looks like this:
--- [2018-10-27T16:32:41] [Server thread/INFO]: LambdaCube[/172.17.0.1:49542] logged in with entity id 452 at (-126.5, 79.0, 216.5)
--- [2018-10-27T16:32:41] [Server thread/INFO]: LambdaCube joined the game
--- [2018-10-27T16:36:12] [Server thread/INFO]: LambdaCube lost connection: Disconnected
--- [2018-10-27T16:36:12] [Server thread/INFO]: LambdaCube left the game
-
--- If the timestamp on the most recent logout is more than some grace period 
--- time in the past, like say, more than 10 minutes ago (configurable), then
--- shutdown the server with this command.
--- rcon-cli stop
+addSeconds :: Real a => a -> LocalTime -> LocalTime
+addSeconds s = utcToLocalTime utc
+             . addUTCTime (realToFrac s)
+             . localTimeToUTC utc
